609f9fdf9d1c3d0f0935e5e8e412864e9879c35b
PetrSkalicky <petrskali@gmail.com>
2025-07-30 18:05:44 +0200

    update

--- kata/narcissistic_number/top_kata_solution.c
+++ kata/narcissistic_number/top_kata_solution.c
@@ -1,12 +1,19 @@
 #include <stdbool.h>
 #include <math.h>
-
 bool narcissistic(int num)
 {
-    int p = log10(num) + 1; // can be off
+    int p = log10(num) + 1;
     int n = num;
     do
         num -= pow(n % 10, p);
     while (n /= 10);
     return !num;
+}
+
+int main(void)
+{
+    // Example usage
+    int number = 153;
+    narcissistic(number);
+    return 0;
 }
